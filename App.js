1d9634d20ff6a2c775babfcac1d85d0d45f99427
Innovation Team <inoovation@Innovations-MacBook-Air.local>
2022-04-11 16:29:08 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, TextInput } from 'react-native';
+
+
+export const App = () => {
+
+  const [nameText, setNameText] = useState('')
+  const textUpdate = (text) => {
+    setNameText(text)
+  }
+
+  const [ageText, setAgeText] = useState('')
+  const AgeUpdate = (text) => {
+    setAgeText(text)
+  }
+
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.container}>
+      <Text style={styles.title}>My Name : {nameText}</Text>
+      <Text style={styles.title}>My Age : {ageText} y/o</Text>
+      <TextInput
+        placeholder="Name"
+        onChangeText={textUpdate}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Age"
+        onChangeText={AgeUpdate}
+        style={styles.input}
+        keyboardType='numeric'
+      />
+      </View>
+    </SafeAreaView>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex:1
+  },
+  container: {
+    backgroundColor:'skyblue',
+    flex:1,
+  },
+  title: {
+    fontSize: 40,
+    color: '#4d4d4d',
+    marginStart:10
+  },
+  input:{
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
+});
