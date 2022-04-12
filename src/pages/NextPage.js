80d4a6f82c6e5a6bd9e50985b2de159f637649e9
shukrij <shukrijasin@gmail.com>
2022-04-12 14:15:04 +0800

    error fixed

--- src/pages/NextPage.js
+++ src/pages/NextPage.js
@@ -4,7 +4,7 @@ import { SafeAreaView, View, Text } from 'react-native';
 export const NextPage = () => {
     return (
         <SafeAreaView style={{flex:1, alignItems:'center'}}>
-          <View style={{flex:1,justifyContent:'space-around',}}>
+          <View style={{flex:1,justifyContent:'space-around'}}>
             <Text>Hello Next Page</Text>
           </View>
         </SafeAreaView>
